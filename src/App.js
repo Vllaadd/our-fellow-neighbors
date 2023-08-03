5c576e1911c4d7ae38b1b715b38a0282f5b102a0
Vllaadd <vlladdiimiir@gmail.com>
2023-08-03 03:06:07 -0400

    database array  missing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Banner from './components/Banner';
+import Banner from './components/Banner'
 import Menu from './components/Menu'
 import Home from './pages/Home'
 
 
 function App() {
+
+  const treesData = 
+  { lat: 40.123, lng: -73.456, type: 'Japanese Zelkova' }
+
   return (
     <>
     <Banner />
     <Menu />
-    <Home />
+    <Home treesData={treesData}/>
     </>
   );
 }
