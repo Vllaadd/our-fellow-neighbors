0c4da7723faa22afb12271c7d038854af56aa163
Vllaadd <vlladdiimiir@gmail.com>
2023-08-04 02:37:45 -0400

    database extended

--- src/components/treeData.js
+++ src/components/treeData.js
@@ -1,8 +1,27 @@
 const treeData = [
-    { lat: 40.123, lng: -73.456, type: 'Japanese Zelkova' },
-    { lat: 42.123, lng: -75.456, type: 'London Plane Tree' },
-    { lat: 48.123, lng: -78.456, type: 'Norway Maple' },
-    { lat: 50.123, lng: -83.456, type: 'Oak' },
-]
+    { lat: 40.674586, lng: -73.961682, type: 'Japanese Zelkova' }, // Brooklyn Botanic Garden
+    { lat: 40.694756, lng: -73.975830, type: 'London Plane Tree' }, // Prospect Park
+    { lat: 40.688163, lng: -73.974073, type: 'Norway Maple' }, // Grand Army Plaza
+    { lat: 40.698077, lng: -73.967489, type: 'Oak' }, // Williamsburg
+    { lat: 40.683128, lng: -73.974932, type: 'American Elm' }, // Brooklyn Bridge Park
+    { lat: 40.679772, lng: -73.975377, type: 'Tulip Tree' }, // Brooklyn Heights
+    { lat: 40.662100, lng: -73.976751, type: 'Red Maple' }, // Park Slope
+    { lat: 40.660968, lng: -73.981799, type: 'White Oak' }, // Gowanus
+    { lat: 40.693921, lng: -73.990115, type: 'Black Cherry' }, // DUMBO
+    { lat: 40.655648, lng: -73.958816, type: 'Eastern Redcedar' }, // Red Hook
+    { lat: 40.688273, lng: -73.974238, type: 'Sugar Maple' }, // Prospect Park
+    { lat: 40.650619, lng: -73.962350, type: 'Sassafras' }, // Carroll Gardens
+    { lat: 40.691829, lng: -73.998659, type: 'Hackberry' }, // Brooklyn Heights
+    { lat: 40.679772, lng: -73.974932, type: 'Bald Cypress' }, // Brooklyn Bridge Park
+    { lat: 40.664876, lng: -73.991946, type: 'Red Horse Chestnut' }, // Boerum Hill
+    { lat: 40.677726, lng: -73.961932, type: 'Silver Maple' }, // Cobble Hill
+    { lat: 40.660641, lng: -73.961579, type: 'Honey Locust' }, // Carroll Gardens
+    { lat: 40.697128, lng: -73.972063, type: 'Black Tupelo' }, // Greenpoint
+    { lat: 40.676482, lng: -73.981926, type: 'Basswood' }, // Fort Greene
+    { lat: 40.669719, lng: -73.960290, type: 'Southern Magnolia' }, // Gowanus
+    { lat: 40.700247, lng: -73.983698, type: 'Buckeye' }, // Williamsburg
+    { lat: 40.685735, lng: -73.965118, type: 'Pignut Hickory' }, // Brooklyn Heights
+  ];
+  
+  export default treeData;
 
-export default treeData;
