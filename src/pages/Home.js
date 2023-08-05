c67b0729ca9ad8920cfd39158c7c9f379e9441bb
Vllaadd <vlladdiimiir@gmail.com>
2023-08-05 00:41:39 -0400

    only marked tree shown on map

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,10 +11,10 @@ const Home = ({ treesData }) => {
         setSearchQuery(event.target.value);
 
         const filteredList = treesData.filter((tree) =>
-        tree.type.toLowerCase().includes(event.target.value.toLowerCase())
-      );
-  
-      setFilteredItems(filteredList);
+            tree.type.toLowerCase().includes(event.target.value.toLowerCase())
+        );
+
+        setFilteredItems(filteredList);
     }
 
     const handleTreeClick = (tree) => {
@@ -33,43 +33,42 @@ const Home = ({ treesData }) => {
     return (
         <div className="container">
             <div className="row">
-            <div className="col-md-6">
-                <div className="row">
-                    <div className="input-group mb-3">
-                        <input type="text" class="form-control" placeholder="Find Your Tree" aria-label="Find Your Tree" aria-describedby="button-addon2" onChange={handleSearchChange} value={searchQuery}></input>
+                <div className="col-md-6">
+                    <div className="row">
+                        <div className="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Find Your Tree" aria-label="Find Your Tree" aria-describedby="button-addon2" onChange={handleSearchChange} value={searchQuery}></input>
+                        </div>
+                        <div className="tree-list">
+                            <ul className="list-group">
+                                {filterItems.map((tree, index) => (
+                                    <li key={index} className="list-group-item" onClick={() => handleTreeClick(tree)}>{tree.type}</li>
+                                ))}
+                            </ul>
+                        </div>
+
                     </div>
-                    <div className="tree-list">
-                    <ul className="list-group">
-                      {filterItems.map((tree, index) => (
-                        <li key={index} className="list-group-item" onClick={() => handleTreeClick(tree)}>{tree.type}</li>
-                      ))}
-                    </ul>
-                    </div>
-                    
                 </div>
-            </div>
-            <div className="col-md-6">
-                <div className="google-map">
-                    <LoadScript
-                        googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
-                    >
-                        <GoogleMap
-                            mapContainerStyle={mapContainerStyle}
-                            center={center}
-                            zoom={11}
+                <div className="col-md-6">
+                    <div className="google-map">
+                        <LoadScript
+                            googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
                         >
-                            {filterItems.map((tree, index) => (
-                                <Marker
-                                    key={index}
-                                    position={{ lat: selectedTree.lat, lng: selectedTree.lng }}
-                                    title={selectedTree.type}
-                                />
-                            ))}
-                        </GoogleMap>
-                    </LoadScript>
+                            <GoogleMap
+                                mapContainerStyle={mapContainerStyle}
+                                center={center}
+                                zoom={11}
+                            >
+                                {selectedTree && (
+                                    <Marker
+                                        position={{ lat: selectedTree.lat, lng: selectedTree.lng }}
+                                        title={selectedTree.type}
+                                    />
+                                )}
+                            </GoogleMap>
+                        </LoadScript>
+                    </div>
                 </div>
             </div>
-        </div>
         </div>
     )
 }
