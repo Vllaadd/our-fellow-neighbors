216276bf24d34aa67cc02e7a5015dd6c9a7f19a7
Vllaadd <vlladdiimiir@gmail.com>
2023-08-16 03:10:39 -0400

    routes issues fixed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Home from './pages/Home';
 import HomeTrees from './pages/HomeTrees';
 import HomeFish from './pages/HomeFish';
 
-
 function App() {
-
   return (
     <Router>
-      <Route path='/home'>
-              <Home />
-        </Route>
-        <Route path='/trees'>
-              <HomeTrees />
-        </Route>
-        <Route path='/fish'>
-              <HomeFish />
-        </Route>
-        <Route path='/animals'>
-              <HomeFish />
-        </Route>
-        <Route path='/birds'>
-              <HomeFish />
-        </Route>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/trees" element={<HomeTrees />} />
+        <Route path="/fish" element={<HomeFish />} />
+      </Routes>
     </Router>
-  ); 
-   
+  );
 }
 
 export default App;
