654b3e08b5e4cb3e0d9ce776d2cb0713c93a7659
Vllaadd <vlladdiimiir@gmail.com>
2023-08-17 01:45:32 -0400

    AboutFish created

--- src/pages/AboutFish.js
+++ src/pages/AboutFish.js
@@ -2,9 +2,14 @@ import React from "react";
 
 const AboutFish = ({fishie}) => {
     return (
-        <dib>
+        <div>
             <h1>{fishie.name}</h1>
-        </dib>
+            <p><strong>Name:</strong>{tree.about.Name}</p>
+            <p><strong>Behavior and Diet:</strong>{fishie.behaviorAndDiet}</p>
+            <p><strong>Life Span:</strong>{fishie.Lifespan}</p>
+            <p><strong>Lifespan:</strong>{fishie.reproductionAndLifeCycle}</p>
+            <p><strong>Fun Fact:</strong>{fishie.funFact}</p>
+        </div>
     )
 }
 
