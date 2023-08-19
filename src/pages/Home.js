dfc9c67a2369f5aa76845601ea8454c1f60cf38c
Vllaadd <vlladdiimiir@gmail.com>
2023-08-19 05:22:03 -0400

    welcome text style done for now

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,24 +3,24 @@ import '../pages/css/Home.css';
 
 const Home = () => {
     return (
-        <div className="container">
-            <div className="welcome-title">
-            <h3>Hello neighbor! </h3>
-            </div>
-                <br/>
-                <br/>
-                <div className="welcome-paragraph">
-                <h5>I created this page in order to practice 
-                    my coding skills, but also out of love and 
-                    respect for nature which is richer and more 
-                    diverse in this concrete jungle than most 
-                    people think.</h5> 
-                    <br />
-                    <h5>Please take a look and get 
-                    familiar with all those beautiful non-humans 
-                    fellow non-human New Yorkers.</h5>
+        <div class="container">
+            <div class="row">
+                <div className="col-md-4"></div>
+                <div className="col-md-8">
+                <div className="welcome-title">
+                        <h3>Hello neighbor! </h3>
+                    </div>
+                    <div className="welcome-paragraph">
+                        <p>I created this website in order to practice
+                            my coding skills, but also out of love and
+                            respect for nature which is richer and more
+                            diverse in this concrete jungle than most
+                            people think. Please go through pages and get
+                            familiar with all those beautiful non-human
+                            fellow New Yorkers.</p>
+                    </div>
                 </div>
-            
+            </div>
         </div>
     )
 }
