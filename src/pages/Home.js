53716ceeaedbfe03c46b88f1981cefcc756135d4
Vllaadd <vlladdiimiir@gmail.com>
2023-08-19 02:56:13 -0400

    text changed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,24 +5,20 @@ const Home = () => {
     return (
         <div className="container">
             <div className="welcome-title">
-            <h3>Hello fellow New Yorkers
-                and everyone interested in our
-                great and diverse city! </h3>
+            <h3>Hello neighbor! </h3>
             </div>
                 <br/>
                 <br/>
                 <div className="welcome-paragraph">
-                <h5>This page has a mission to raise awareness
-                about the importance of maintaining balance with our
-                non-human neighbors. While New York City is globally
-                renowned for its cultural diversity, what often goes
-                unnoticed is the rich diversity of flora and fauna
-                that we coexist with in this urban landscape.
-                This webpage is a work in progress, and I assure you that
-                I'll continuously enhance the design and expand the database.
-                Take a look at the menu and delve into the vibrant and varied
-                world of animals, fish, birds, and trees that share this
-                incredible city with us humans.</h5>
+                <h5>I created this page in order to practice 
+                    my coding skills, but also out of love and 
+                    respect for nature which is richer and more 
+                    diverse in this concrete jungle than most 
+                    people think.</h5> 
+                    <br />
+                    <h5>Please take a look and get 
+                    familiar with all those beautiful non-humans 
+                    fellow non-human New Yorkers.</h5>
                 </div>
             
         </div>
