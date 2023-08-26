ad461589ea332fb235641979e315c0a4930385be
Vllaadd <vlladdiimiir@gmail.com>
2023-08-26 05:51:11 -0400

    contact page styling

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,12 +2,15 @@ import React from "react";
 import './css/Contact.css';
 
 const Contact = () => {
-    return(
-        <div className="contact">
-            <h5>Thank you for visiting the page.</h5>
-            <h5>Please reach out at:</h5>
-            <p><a href="mailto:vladzizic@gmail.com">vladzizic@gmail.com</a></p>
+    return (
+        <div className="container">
+            <div className="contact">
+                <h5>Thank you for visiting the page.</h5>
+                <h5>Please reach out at:</h5>
+                <p><a href="mailto:vladzizic@gmail.com">vladzizic@gmail.com</a></p>
+            </div>
         </div>
+            
     )
 }
 
