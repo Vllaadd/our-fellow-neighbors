32fe29c4f96349b011f158daeb1f102f54259fc9
Vllaadd <vlladdiimiir@gmail.com>
2023-11-22 05:24:40 -0500

    node environment check

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// checking if the app is running in development mode for deployment purposes 
+if (process.env.NODE_ENV === 'production') {
+  console.log('Running in production mode');
+} else {
+  console.log('Running in development mode');
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
